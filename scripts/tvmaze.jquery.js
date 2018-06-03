04b10936e48dbe8cf2ee7f4c2ebc5f619eaaefa5
apoorvanand <apoorwanand@gmail.com>
2018-06-03 07:04:01 +0530

    Update tvmaze.jquery.js

--- scripts/tvmaze.jquery.js
+++ scripts/tvmaze.jquery.js
@@ -3,7 +3,7 @@
         $.tvmaze = {
             getEpisode: function(showId,  seasonNumber, episodeNumber, callBack){
                 var req = $.ajax({
-                    url:'http://api.tvmaze.com/shows/' + showId + '/episodebynumber?season=' + seasonNumber + '&number=' + episodeNumber
+                    url:'https://api.tvmaze.com/shows/' + showId + '/episodebynumber?season=' + seasonNumber + '&number=' + episodeNumber
                 });
         
                 req.done(callBack);
@@ -11,7 +11,7 @@
 
             getEpisodes: function(showId, callBack){
                 var req = $.ajax({
-                    url:'http://api.tvmaze.com/shows/' + showId + '/episodes'
+                    url:'https://api.tvmaze.com/shows/' + showId + '/episodes'
                 });
         
                  req.done(callBack);
@@ -19,7 +19,7 @@
 
             getShow: function(showName, callBack){
                 var req = $.ajax({
-                    url:'http://api.tvmaze.com/search/shows/?q=' + showName
+                    url:'https://api.tvmaze.com/search/shows/?q=' + showName
                 });
         
                  req.done(callBack);
